2beb5faec08a22458b1d0f95a8e35f775476a98f
Mohammed Ja <m.hadi@jisr.net>
2025-10-21 16:02:43 +0300

    chore: integarate with APIs

--- src/services/api.ts
+++ src/services/api.ts
@@ -9,7 +9,7 @@ import type {
 
 // Create axios instance with base configuration
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://todo-be-lac.vercel.app/api",
   headers: {
     "Content-Type": "application/json",
   },
